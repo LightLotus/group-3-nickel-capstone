ddfa9cfa87f254f1b4392de80a1205e0b51fe6b4
kevin <kevinsef94@gmail.com>
2022-09-01 16:22:29 +0800

    created register section

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -1,5 +1,52 @@
+import "../css/Register.css";
+import { Link } from "react-router-dom";
+
 const Register = () => {
-  return <div>Register</div>;
+  return (
+    <>
+      <div className="login-container">
+        <div className="login-wrapper">
+          <p className="login-title">Create Account</p>
+          <p>Please fill in the information below</p>
+          <form action="#">
+            <input
+              className="login-input"
+              type="text"
+              placeholder="    First Name"
+            />
+            <input
+              className="login-input"
+              type="text"
+              placeholder="    Last Name"
+            />
+            <input
+              className="login-input"
+              type="emal"
+              placeholder="    Email"
+            />
+            <input
+              className="login-input"
+              type="password"
+              placeholder="   Password"
+            />
+            <button
+              className="button-link-style"
+              id="login-button"
+              type="submit"
+            >
+              Login
+            </button>
+          </form>
+          <p className="dont-have-account">
+            Already have an account?{" "}
+            <Link id="create-account" to="/login">
+              Login here.
+            </Link>
+          </p>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Register;
