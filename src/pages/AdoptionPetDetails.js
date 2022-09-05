7467709acd5563c416529a8358b455eff5ab8db3
kevin <kevinsef94@gmail.com>
2022-09-05 15:46:16 +0800

    added back button in adoption pet details

--- src/pages/AdoptionPetDetails.js
+++ src/pages/AdoptionPetDetails.js
@@ -1,8 +1,11 @@
-
+import { Link } from "react-router-dom"
 const AdoptionPetDetails = () => {
   return (
-    <div>AdoptionPetDetails</div>
-  )
+    <>
+    <Link to="/adoption" className="button-link-style">Back</Link>
+      <div>Adoption Pet Details Section</div>
+    </>
+  );
 }
 
 export default AdoptionPetDetails
